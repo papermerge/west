3cdda97cf627c729ca8d74ecfb7044d02c92a0f1
Eugen Ciur <eugen@papermerge.com>
2024-10-23 11:23:35 +0200

    very basic websockets server

--- west/main.py
+++ west/main.py
@@ -1,15 +1,24 @@
 import asyncio
-import aioredis
+from redis import asyncio as aioredis
 import uuid
 import json
+import urllib.parse
 from websockets.asyncio.server import serve
 
 from west.config import get_settings
 
 
-NOTIFICATIONS = "notifications"
+CHANNEL = "notifications"
 CONNECTIONS = {}
 settings = get_settings()
+
+
+def get_query_param(path, key):
+    query = urllib.parse.urlparse(path).query
+    params = urllib.parse.parse_qs(query)
+    values = params.get(key, [])
+    if len(values) == 1:
+        return values[0]
 
 async def handler(websocket):
     try:
@@ -25,10 +34,10 @@ async def handler(websocket):
         del CONNECTIONS[user_id]
 
 
-async def cookie_auth(connection, request):
+async def remove_user_auth(connection, request):
     # extract user ID from "Remote-User" header
     # or from JWT token in Authorization header
-    user_id = request.headers.get("Remote-User", None)
+    user_id = get_query_param(request.path, "remote-user-id")
     if user_id and isinstance(user_id, str):
         connection.user_id = uuid.UUID(user_id)
 
@@ -38,25 +47,25 @@ async def process_events():
     redis = aioredis.from_url(settings.papermerge__redis__url)
     pubsub = redis.pubsub()
 
-    await pubsub.subscribe(NOTIFICATIONS)
-
+    await pubsub.subscribe(CHANNEL)
+    print(f"Subscribing to {settings.papermerge__redis__url} channel: {CHANNEL}")
     async for message in pubsub.listen():
         if message["type"] != "message":
             continue
 
         payload = message["data"].decode()
-        # Broadcast event to all users who have permissions to see it.
         event = json.loads(payload)
-
         for user_id, websocket in CONNECTIONS.items():
-            if event["user_id"] == user_id:
-               await websocket.send(json.dumps(event))
+            if event['payload']["user_id"] == str(user_id):
+                data = json.dumps(event)
+                print(data)
+                await websocket.send(data)
 
 
 async def main():
-    async with serve(handler, "", 8001, process_request=cookie_auth):
-        await asyncio.get_running_loop().create_future()  # run forever
-
+    print("Starting on port 8001")
+    async with serve(handler, "", 8001, process_request=remove_user_auth):
+        await process_events()  # runs forever
 
 if __name__ == "__main__":
     asyncio.run(main())
